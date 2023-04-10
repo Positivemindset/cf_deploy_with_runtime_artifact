1aac9fd2c3c1ef726a6aead0ab0f4390f614c6b0
Harsha Evuri <harsha.evuri@gmail.com>
2023-04-10 18:13:16 +0100

    updated the python file

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-def hello_world2(request):
-    return "Hello, World!"
+import json
+import requests
+from flask import jsonify
+
+def fetch_data_option2_cf(request):
+    url = 'https://jsonplaceholder.typicode.com/todos/1'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        return jsonify(response.json())
+    else:
+        return jsonify({"error": "Failed to fetch data"}), response.status_code
